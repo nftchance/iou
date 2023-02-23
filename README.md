18fad3ea6711dab7b046be1fa0798946f0ba9805
nftchance <chance@utc24.io>
2023-02-22 20:19:56 -0600

    fix: docs

--- README.md
+++ README.md
@@ -5,3 +5,35 @@
 Powering the functionality of trusted cross-chain payments, IOUs stand as an on-chain representation for an asset owed. Offered by the token distributor, a signature is enforced to provide an opportunity that all tokens minted have funds to be redeemed against.
 
 > This system utilizes a trusted party to provide the IOU, and is not a decentralized solution.
+
+## Building
+
+The dependencies for IOUs are very slim. To install all that is needed, run:
+
+```bash
+npm install
+```
+
+## Testing
+
+This repository is setup to use [Hardhat](https://hardhat.org/), a development environment to compile, deploy, test, and debug your Ethereum software. It helps developers manage and automate the recurring tasks that are inherent to the process of building smart contracts and dApps, as well as easily introducing more functionality around this workflow.
+
+To install the dependencies, run the tests and prepare for development, run:
+
+```bash
+npx hardhat test
+```
+
+## Deploying 
+
+To deploy the project to a local network, run:
+
+```bash
+npx hardhat deploy --network localhost
+```
+
+To deploy the project to a live network, first configure your `.env` based on `example.env` and then run:
+
+```bash
+npx hardhat deploy --network polygon
+```
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,108 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-etherscan");
+require("hardhat-abi-exporter");
 
-/** @type import('hardhat/config').HardhatUserConfig */
+require("dotenv").config();
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+    const accounts = await hre.ethers.getSigners();
+
+    for (const account of accounts) {
+        console.log(account.address);
+    }
+});
+
+
+task("deploy", "Deploys the protocol")
+    .addFlag("verify", "Verify the deployed contracts on Etherscan")
+    .setAction(async (taskArgs, hre) => {
+        await hre.run('compile');
+
+        const [deployer] = await ethers.getSigners();
+        console.log(`✅ Connected to ${deployer.address}`);
+
+        const chainId = await getChainId()
+
+        const IOUFactory = await ethers.getContractFactory("IOUFactory");
+
+        const badgeAddress = "0x0"
+
+        const creationBadge = {
+            token: badgeAddress,
+            id: 0,
+            amount: 1
+        }
+
+        iouFactory = await IOUFactory.deploy(deployer.address, signerAddress, creationBadge);
+        iouFactory = await iouFactory.deployed();
+        console.log("✅ IOUFactory deployed.")
+
+        factoryDeployment = {
+            "Chain ID": chainId,
+            "Deployer": deployer.address,
+            "Organization Implementation Address": iouFactory.address,
+            "Remaining ETH Balance": parseInt((await deployer.getBalance()).toString()) / 1000000000000000000,
+        }
+        console.table(factoryDeployment)
+
+        // Verifying
+        if (taskArgs.verify !== false && chainId != '31337') {
+            // Give time for etherscan to confirm the contract before verifying.
+            await new Promise(r => setTimeout(r, 30000));
+            await hre.run("verify:verify", {
+                address: iouFactory.address,
+                constructorArguments: [
+                    deployer.address,
+                    signerAddress,
+                    creationBadge
+                ],
+            });
+
+            console.log("✅ IOUFactory Verified.")
+        }
+    });
+
+
 module.exports = {
-    solidity: "0.8.16",
+    solidity: {
+        compilers: [
+            {
+                version: "0.8.16",
+                settings: {
+                    optimizer: {
+                        enabled: true,
+                        runs: 1000000
+                    }
+                }
+            }
+        ],
+    },
+    etherscan: {
+        apiKey: {
+            polygon: process.env.ETHERSCAN_KEY,
+        }
+    },
+    gasReporter: {
+        enabled: true,
+        currency: 'USD',
+        gasPrice: 20,
+        coinmarketcap: process.env.COINMARKETCAP_KEY,
+        showMethodSig: true,
+        showTimeSpent: true,
+    },
+    networks: {
+        polygon: {
+            url: `https://polygon-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_KEY}`,
+            accounts: [`0x${process.env.PRIVATE_KEY}`],
+            gasPrice: 'auto'
+        },
+    },
+    abiExporter: {
+        path: './abis/',
+        runOnCompile: true,
+        clear: true,
+        flat: true,
+        spacing: 2,
+        format: "json"
+    }
 };
